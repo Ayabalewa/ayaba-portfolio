8c99bcc04f3b16b7a0b86ac184a3f38340db0e91
AYABA <tobilobamurele@gmail.com>
2022-09-05 12:54:39 +0100

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 const Navbar = () => {
-    return (
-        <nav className="navbar"></nav>
-    ) ;
+  return ( 
+    <nav className="bg-white px-4 py-2.5 rounded ">
+        
+
+    </nav>
+   );
 }
  
-export default Navbar;
+export default Navbar;  
